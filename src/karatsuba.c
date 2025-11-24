745ce709b9d82479bf3ea54a331a9c4633d2a0db
NaVeeDev <laura.ly.3964@gmail.com>
2025-11-24 12:41:43 +0100

    started karatsuba

---
+++ src/karatsuba.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "naive.h"
+
+void Karatsuba (double * P1, double * P2, double * res, int d1, int d2, int k) {
+   /* The algorithm must rely on the naive algorithm when polynomials have size at most k. */
+    if (d1 < k || d2 < k) {
+        poly_multiplication_classic(P1, P2, d1, d2, res);
+    } else {
+        int m = (d1 > d2 ? d1 : d2) / 2;
+        double * P1_ = malloc((m) * sizeof(double));
+        double * P2_ = malloc((m) * sizeof(double));
+
+        for (int i = 0; i < m; i++) {
+            P1_[i] = (P1[i] + P1[m + i]);
+            P2_[i] = (P2[i] + P2[m + i]);
+        }
+        if (d1 > 2*m - 1) {
+            P1_[m] = P1[d1];
+        } else {
+            P1_[m] = 0;
+        }
+        if (d2 > 2*m - 1) {
+            P2_[m] = P2[d2];
+        } else {
+            P2_[m] = 0;
+        }
+
+        double * R1 = malloc((2 * (m-1)) * sizeof(double));
+        double * R2 = malloc((2 * (m-1)) * sizeof(double));
+        double * R3 = malloc((2 * m) * sizeof(double));
+
+        Karatsuba(P1, P2, R1, m, m, k);
+        Karatsuba(P1 + m, P2 + m, R2, d1 - m, d2 - m, k);
+        Karatsuba(P1_, P2_, R3, m + 1, m + 1, k);
+
+        double * R4 = malloc((2 * m) * sizeof(double));
+        for (int i = 0; i < 2 * (m - 1); i++) {
+            R4[i] = R3[i] - R1[i] - R2[i];
+        }
+
+    }
+
+}
